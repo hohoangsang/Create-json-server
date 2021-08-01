c1d07b2da95b22fe7195b083e6483846149ae536
hohoangsang <hoangsangho93@gmail.com>
2021-08-01 16:43:56 +0700

    fix main.js again

--- main.js
+++ main.js
@@ -24,6 +24,6 @@ server.use((req, res, next) => {
  
 // Use default router
 server.use("/api", router)
-app.listen(process.env.PORT || 3000, function(){
+server.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
